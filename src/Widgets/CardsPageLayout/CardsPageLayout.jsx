98c6a2cd1b2aeb755e363cf2567121c4f65853aa
pasha <pashabezk@mail.ru>
2025-06-19 02:04:58 +0300

    feat: display last operation

--- src/Widgets/CardsPageLayout/CardsPageLayout.jsx
+++ src/Widgets/CardsPageLayout/CardsPageLayout.jsx
@@ -1,18 +1,25 @@
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 import {NavLink} from "react-router-dom";
 import {RouterPath} from "../../Shared/Constants/RouterConstants.js";
 import {getRandomUser} from "../../Shared/MockData/UsersData.js";
 import styles from "./CardsPageLayout.module.css";
 import "../../Widgets/UserCard/UserCard.css";
 
+/**
+ * @typedef {{operation?: string, duration?: number}} LastOperation
+ */
+
 const operations = {
-	delete: 'delete',
-	insert: 'insert',
+	delete: "delete",
+	insert: "insert",
 };
 
 const CardsPageLayout = ({cardsData, setCardsData, children}) => {
 	const startTimeRef = useRef(null);
 	const operationRef = useRef(null);
+
+	/** @type {[LastOperation, React.Dispatch<LastOperation>]} */
+	const [lastOperation, setLastOperation] = useState({operation: null, duration: null});
 
 	useEffect(() => {
 		if (startTimeRef.current !== null) {
@@ -27,9 +34,10 @@ const CardsPageLayout = ({cardsData, setCardsData, children}) => {
 					console.log(`Время выполнения операции вставки и обновления интерфейса: ${duration} миллисекунд`);
 					break;
 				default:
-					console.log('Unknown operation');
+					console.log("Unknown operation");
 			}
 
+			setLastOperation({operation: operationRef.current, duration});
 			startTimeRef.current = null;
 			operationRef.current = null;
 		}
@@ -57,6 +65,17 @@ const CardsPageLayout = ({cardsData, setCardsData, children}) => {
 			<NavLink to={RouterPath.Main}>На главную</NavLink>
 			<div>
 				<p>Rendered items: {cardsData.length}</p>
+				<p>
+					Last operation:{" "}
+					{
+						lastOperation.operation
+							? <>
+								<span data-test-id="last-operaion">{lastOperation.operation}</span>{" "}
+								(<span data-test-id="last-operaion-duration">{lastOperation.duration.toFixed(3)}</span> ms)
+							</>
+							: <>null</>
+					}
+				</p>
 				<button onClick={addItem}>Insert</button>
 				<button onClick={deleteItem}>Delete</button>
 			</div>
